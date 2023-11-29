b4f73b941d77c5073456d3a2088c8c04d8293109
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2023-11-30 04:51:04 +0530

    deploy

---
+++ client/build/precache-manifest.25269a161971be1033991822e3196533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a13b3907545d7e437056380947040ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09bc94101668e6a6c4f",
+    "url": "/static/css/main.23aec1d5.chunk.css"
+  },
+  {
+    "revision": "20c611996d439a03df52",
+    "url": "/static/js/2.62023cb0.chunk.js"
+  },
+  {
+    "revision": "91efb7b759cc0a54fac8775cb7b254e2",
+    "url": "/static/js/2.62023cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09bc94101668e6a6c4f",
+    "url": "/static/js/main.f1276e30.chunk.js"
+  },
+  {
+    "revision": "3361b6d09811b23691c5",
+    "url": "/static/js/runtime-main.85a5c5c8.js"
+  },
+  {
+    "revision": "2e1963be63bb88eada0884f589443fbb",
+    "url": "/static/media/haomin-bg-redteal.2e1963be.PNG"
+  }
+]);
